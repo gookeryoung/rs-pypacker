0e6c3815515ab213eba96b506468e5816628654b
Young <gooker_young@163.com>
2024-10-23 21:18:03 +0800

    ~

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 use std::env;
-use std::ffi;
 
 fn main() {
-    let args: Vec<ffi::OsString> = env::args_os().collect();
-    dbg!(args);
+    let args: Vec<String> = env::args().collect();
+    println!("[*] args={:?}", args);
 
     if args.len() < 2 {
         println!("[*] 选择当前路径作为项目源目录");
+    } else {
+        println!("args[0]={}, args[1]={}", args[0], args[1]);
     }
-
-    println!("Hello, world!");
 }
